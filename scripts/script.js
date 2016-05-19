d954f79de7d04f3473b5bcece014cfdef0ab1879
cfcrawford <camilafcrawford@gmail.com>
2016-05-19 15:58:54 -0700

    Adds point counter and disable click event in script

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,9 @@
 var turn = 0;
+var firstElement;
+var point = 0;
 //var score = 0;
 var squares = document.getElementsByTagName('div');
-// var div1 = document.getElementById('div1');
-// var div2 = document.getElementById('div2');
-// var div3 = document.getElementById('div3');
-// var div4 = document.getElementById('div4');
-// var div5 = document.getElementById('div5');
-// var div6 = document.getElementById('div6');
-// var div7 = document.getElementById('div7');
-// var div8 = document.getElementById('div8');
+
 
 // var bgImageArray = [
 // "http://www.entertainmentearth.com/images/AUTOIMAGES/RBPIN008lg.jpg",
@@ -23,42 +18,102 @@ window.onload = function() {
 }
 
 function changeImage() {
+  turn += 1;
   var currentElement = this.id;
 
   var currentCell = document.getElementById(currentElement);
-  var currentElementString = "" + currentElement
+  var currentElementString = "" + currentElement;
   var currentCellClass = currentElementString.slice(0, 4);
-  // console.log(currentCellClass);
-  currentCell.classList.add(currentCellClass);
+  // console.log(currentCell);
+  currentCell.className += " " + currentCellClass;
+  currentCell.removeEventListener("click", changeImage);
 
-  // var link = bgImageArray[currentElementNumber];
-
-  // currentElement.setAttribute("style", "background-image: ", link);
-  //how do I specify the first square I click and the second one?
-
-  /*if (squares[i].div == squares[i + 1]) {
-    function Addpoint();
+  if (turn % 2 !== 0){
+    console.log("first turn");
+    firstElement = currentCellClass;
+    console.log(firstElement);
   }
-  else {
-
-  }*/
-
-
-}
-
-function addPoint() {
+  if (turn % 2 === 0){
+    // compare classes
+    compareClasses(currentCellClass, firstElement);
+  }
 
 }
 
-/* function changeTurn() {
+function compareClasses(currentElement, firstElement) {
+  // console.log("even number of turns");
+  console.log('current element is ', currentElement);
+  console.log('first element is ', firstElement);
+  if (currentElement == firstElement) {
+    console.log('they are the same');
+    // add points
+    // disable tiles
+    turn = 0;
+    point ++;
+    recordPoint();
+
+  } else {
+    console.log('they are different');
+    resetTiles(currentElement, firstElement);
+    turn = 0;
+
+  }
+}
+
+
+
+function resetTiles(currentElement, firstElement) {
+  if (turn > 1) {
+    // var selectedTiles = document.getElementsByClassName("div");
+    // console.log(selectedTiles);
+
+    // replace .classList.remove() with .className = "col"
+    currentElement.classList.remove(currentCellClass);
+    firstElement.classList.remove(currentCellClass);
+// then set the value / properties to zero etc
+    //selectedTiles.innerHTML = "";
+    //document.getElementById("selectedTiles").innerHTML = "";
+
+  }
+}
+
+function recordPoint() {
+  document.getElementById("score").innerHTML = "Score is: " +  point;
+  document.getElementById("messages").innerHTML = "You scored one point!"
+}
+
+
+/* function endGame() {
+  if  (clicks = 16) {
+    resetTiles();
+  }
+}
+
+
+function clearBox() {
+  document.getElementById("currentElement").innerHTML = "";
+}
+
+
+function changeTurn() {
   // Each square is assigned an image
   // One click makes image appear
   // Second click makes another image appear
   // Disallow any more clicks (disable?)
   // Check if both images are identical
   // If images are identical they stay up
-  // If images aren't they turn over
+  // If images arent they turn over
   if (turn == 0) {
     squares[i].style.backgroundImage =
   }
 } */
+
+
+// var div1 = document.getElementById('div1');
+// var div2 = document.getElementById('div2');
+// var div3 = document.getElementById('div3');
+// var div4 = document.getElementById('div4');
+// var div5 = document.getElementById('div5');
+// var div6 = document.getElementById('div6');
+// var div7 = document.getElementById('div7');
+// var div8 = document.getElementById('div8');
